5b65c4dc456c96b23c8fcecb408536ea7595f7b0
yaozeyuan <yaozeyuan93@gmail.com>
2023-11-26 16:22:28 +0800

    feat: 代码检查通过, 待实际运行测试

--- script/php_use_demo/static.php
+++ script/php_use_demo/static.php
@@ -6,9 +6,14 @@ class StaticRes
     static protected $config;
     static protected $mapJson;
     // 需要指定manifest.json的路径. 支持本地路径/cdn地址两种形式
-    static protected $manifestUri = "/demo/0.0.3/manifest.json"
+    static protected $manifestUri = "/demo/0.0.3/manifest.json";
+
+
     // 如果是cdn配置, 会将下载后的manifest.json文件缓存在这里方便后续读取. 默认使用系统缓存路径
-    static protected $cacheDir = sys_get_temp_dir() + "/map-json/";
+    static private function getCacheDir(){
+        return sys_get_temp_dir() . DIRECTORY_SEPARATOR .  "map-json";
+    }
+
 
     /**
      * @return StaticRes
@@ -22,8 +27,8 @@ class StaticRes
         if (empty(self::$inst)) {
             self::$inst = new self();
         }
-        if (!file_exists(self::$cacheDir)) {
-            mkdir(self::$cacheDir, 0777, true);
+        if (!file_exists($this->getCacheDir())) {
+            mkdir($this->getCacheDir(), 0777, true);
         }
         return self::$inst;
     }
@@ -44,7 +49,7 @@ class StaticRes
         // <link rel="stylesheet" crossorigin href="/assets/home-FdWSi62J.css">
         if(empty(self::$config)){
             // 首次启动需要加载manifest.json配置
-            $this->getAutoStaticByFile()
+            $this->getAutoStaticByFile();
         }
 
 
@@ -53,18 +58,18 @@ class StaticRes
 
         $staticList = [];
         // 添加静态资源地址
-        $staticList[] = "<script type='module' crossorigin  src='{$staticConfig["file"]}'></script>"
+        $staticList[] = "<script type='module' crossorigin  src='{$staticConfig["file"]}'></script>";
         if(empty($staticConfig['imports']) === false){
             // 外部以来不为空
             foreach($staticConfig['imports'] as $subImportKey){
                 // 添加额外导入的静态资源地址
                 $subImportConfig = self::$config[$subImportKey];
                 // 添加外部依赖的js
-                $staticList[] = "<link rel='modulepreload' crossorigin href='{$subImportConfig["file"]}'>"
+                $staticList[] = "<link rel='modulepreload' crossorigin href='{$subImportConfig["file"]}'>";
                 if(empty($subImportConfig['css']) === false){
                     // 补充依赖的css
                     foreach($subImportConfig['css'] as $subImportCss){
-                        $staticList[] = "<link rel='stylesheet' crossorigin href='{$subImportCss}'>"
+                        $staticList[] = "<link rel='stylesheet' crossorigin href='{$subImportCss}'>";
                     }
                 }
             }
@@ -73,14 +78,14 @@ class StaticRes
         if(empty($staticConfig['css']) === false){
             // 补充依赖的css
             foreach($staticConfig['css'] as $staticImportCss){
-                $staticList[] = "<link rel='stylesheet' crossorigin href='{$staticImportCss}'>"
+                $staticList[] = "<link rel='stylesheet' crossorigin href='{$staticImportCss}'>";
             }
         }
 
         // 合成为需要注入到html的静态资源代码
         $result = '';
         if(empty($staticList) === false){
-            $result = implode("\n", $staticList)
+            $result = implode("\n", $staticList);
         }
 
         // 试过很多方法，暂时没办法判断请求是http还是https, 故如果页面支持https则使用//进行自适应匹配.
@@ -99,7 +104,7 @@ class StaticRes
      */
     protected function getAutoStaticByFile($static_url)
     {
-        $filePath = self::$cacheDir . md5($static_url) . '.json';
+        $filePath = $this->getCacheDir() . md5($static_url) . '.json';
         if (file_exists($filePath)) {
             $mapJson = file_get_contents($filePath);
             $mapJson = json_decode($mapJson, true);
